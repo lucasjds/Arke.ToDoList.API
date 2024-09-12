1d1ed73678d0c63ce363b508093a80e2cef863c7
lucasjds <lucassouza690@gmail.com>
2024-09-12 15:30:39 -0300

    creating TaskProfile for mapping

---
+++ Arke.ToDoList.API/Mappings/TaskProfile.cs
@@ -0,0 +1,15 @@
+﻿using Arke.ToDoList.API.DataAccess.Entities;
+using Arke.ToDoList.API.Models;
+using AutoMapper;
+
+namespace Arke.ToDoList.API.Mappings
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<TaskEntity, TaskModel>();
+            CreateMap<TaskModel, TaskEntity>();
+        }
+    }
+}
--- Arke.ToDoList.API/Models/TaskModel.cs
+++ Arke.ToDoList.API/Models/TaskModel.cs
@@ -1,6 +1,11 @@
-﻿namespace Arke.ToDoList.API.Models
+﻿using Arke.ToDoList.API.Enums;
+
+namespace Arke.ToDoList.API.Models;
+
+public class TaskModel
 {
-    public class TaskModel
-    {
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public TaskStatusEnum Status { get; set; }
 }
