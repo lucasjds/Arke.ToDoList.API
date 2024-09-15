d70d99cd2fa1b828b6faa1fcc104352b41b39201
lucasjds <lucassouza690@gmail.com>
2024-09-15 13:08:21 -0300

    removing property

--- Arke.ToDoList.API.Domain/Entities/TaskEntity.cs
+++ Arke.ToDoList.API.Domain/Entities/TaskEntity.cs
@@ -9,5 +9,4 @@ public class TaskEntity : IEntity
     public string Name { get; set; }
     public string Description { get; set; }
     public TaskStatusEnum Status { get; set; }
-    public DateTime Created { get; set; }
 }
